8b79c30c8632bcba76da5ebe5ff29576c57fc916
Baba Dorin <bvd.dorin@gmail.com>
2020-10-01 20:03:07 +0300

    Building SeeModifyForm

--- InfoCollector/Forms/SeeModifyForm.Designer.cs
+++ InfoCollector/Forms/SeeModifyForm.Designer.cs
@@ -31,6 +31,9 @@
             this.btGoBack = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panelData = new System.Windows.Forms.Panel();
+            this.paddingLeft = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,11 +64,36 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
             this.label1.Location = new System.Drawing.Point(289, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(183, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "System information";
+            // 
+            // panelData
+            // 
+            this.panelData.AutoScroll = true;
+            this.panelData.Location = new System.Drawing.Point(122, 80);
+            this.panelData.Name = "panelData";
+            this.panelData.Size = new System.Drawing.Size(526, 397);
+            this.panelData.TabIndex = 2;
+            // 
+            // paddingLeft
+            // 
+            this.paddingLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.paddingLeft.Location = new System.Drawing.Point(0, 74);
+            this.paddingLeft.Name = "paddingLeft";
+            this.paddingLeft.Size = new System.Drawing.Size(116, 403);
+            this.paddingLeft.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(654, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(103, 403);
+            this.panel2.TabIndex = 4;
             // 
             // SeeModifyForm
             // 
@@ -73,6 +101,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(37)))), ((int)(((byte)(42)))));
             this.ClientSize = new System.Drawing.Size(757, 477);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.paddingLeft);
+            this.Controls.Add(this.panelData);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(242)))), ((int)(((byte)(241)))));
@@ -90,5 +121,8 @@
         private System.Windows.Forms.Button btGoBack;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelData;
+        private System.Windows.Forms.Panel paddingLeft;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- InfoCollector/Forms/SeeModifyForm.cs
+++ InfoCollector/Forms/SeeModifyForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InfoCollector.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,18 +13,92 @@ namespace InfoCollector.Forms
 {
     public partial class SeeModifyForm : Form
     {
+        struct Unit
+        {
+            public string Identifier;
+            public string Value;
+            public bool isTitle;
+        }
+
         private Form1 parentInstance;
+        
         public SeeModifyForm(Form1 parent)
         {
             this.parentInstance = parent;
             InitializeComponent();
             label1.Left = this.Width / 2 - label1.Width / 2;
+
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            Computer computer = Computer.GetInstance();
+            List<Unit> units = new List<Unit>();
+
+            // General information
+            units.Add(new Unit { Identifier = "ComputerId", Value = computer.ComputerID });
+            units.Add(new Unit { Identifier = "TempName", Value = computer.TempName});
+            
+            // CPU
+            units.Add(new Unit { Identifier = "CPU", isTitle = true});
+            units.Add(new Unit { Identifier = "Manufacturer", Value = computer.CPU.Manufacturer });
+            units.Add(new Unit { Identifier = "Name", Value = computer.CPU.Name });
+            units.Add(new Unit { Identifier = "Architecture", Value = computer.CPU.Architecture });
+            units.Add(new Unit { Identifier = "Number of Cores", Value = computer.CPU.NumberOfCores });
+            units.Add(new Unit { Identifier = "ProcessorId", Value = computer.CPU.ProcessorId });
+            units.Add(new Unit { Identifier = "Status", Value = computer.CPU.Status });
+            units.Add(new Unit { Identifier = "Description", Value = computer.CPU.Description });
+
+
+            for(int i = units.Count - 1; i >= 0; i--)
+            {
+                DisplayUnit(units[i].Identifier, units[i].Value, units[i].isTitle);
+            }
+        }
+
+        private void DisplayUnit(string label, string value, bool isTitle)
+        {
+            Panel unitPanel = new Panel();
+            unitPanel.Dock = DockStyle.Top;
+            unitPanel.Height = 55;
+
+            // ex: ComputerID
+            Label unitIndentifier = new Label();
+            unitIndentifier.Text = label;
+            unitIndentifier.Font = (isTitle) ? new Font("Tahoma", 13, FontStyle.Bold) : new Font("Tahoma", 10, FontStyle.Bold);
+            unitIndentifier.Dock = DockStyle.Top;
+            unitIndentifier.TextAlign = ContentAlignment.MiddleCenter;
+            unitIndentifier.Height = 20;
+
+            if (isTitle)
+            {
+                unitIndentifier.Dock = DockStyle.Fill;
+                unitPanel.Controls.Add(unitIndentifier);
+                panelData.Controls.Add(unitPanel);
+                return;
+            }
+
+            // ex: L23002X
+            TextBox unitValue = new TextBox();
+            unitValue.Dock = DockStyle.Top;
+            unitValue.Text = value;
+            unitValue.Font = new Font("Tahoma", 10);
+            unitValue.BackColor = Color.FromArgb(23, 37, 42);
+            unitValue.ForeColor = Color.FromArgb(222, 242, 241);
+            unitValue.TextAlign = HorizontalAlignment.Center;
+            unitValue.BackColor = Color.FromArgb(23, 37, 42);
+
+            unitPanel.Controls.Add(unitValue);
+            unitPanel.Controls.Add(unitIndentifier);
+            
+            panelData.Controls.Add(unitPanel);
         }
 
         private void btGoBack_Click(object sender, EventArgs e)
         {
             parentInstance.TogglePanelExport();
-            this.Close();
+            this.Dispose();
         }
     }
 }
